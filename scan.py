d2a934ef5acd2eb76146f961dcdb89087e9369c8
Shihab Suliman <rye74444@ws557.diamond.ac.uk>
2024-07-24 13:39:42 +0100

    Added prepare plan for meca in scan.py

--- scan.py
+++ scan.py
@@ -7,6 +7,9 @@ from bluesky import RunEngine
 RE = RunEngine()
 
 class Robot(Device):
+    connect = Cpt(EpicsSignal, 'Connect')
+    activate = Cpt(EpicsSignal, 'Activate')
+    home = Cpt(EpicsSignal, 'Home')
     buffer_step = Cpt(EpicsSignal, 'BufferSize')
     crossover = Cpt(EpicsSignal, 'BufferCross')
     frequency = Cpt(EpicsSignal, 'Frequency')
@@ -15,25 +18,30 @@ class Robot(Device):
     buffer = Cpt(EpicsSignal, 'BufferAll.PROC')
     
     checkpoint = Cpt(EpicsSignalRO, 'Checkpoint')
-    capture = 0
 
 class Camera(Device):
     acquire = Cpt(EpicsSignal, 'Acquire')
+    capture = 0
 
 my_robot = Robot('mecaRobot:', name = 'my_robot', read_attrs = ["checkpoint"])
 my_camera = Camera('LA84R-DI-DCAM-01:CAM:', name = 'my_camera')
 
+def prepare():
+    yield from mv(my_robot.connect, 1)
+    yield from mv(my_robot.activate, 1)
+    yield from mv(my_robot.home, 1)
+
+def capture(**kwargs):
+    if my_camera.capture:
+        my_camera.acquire.put(1)
+
 def scan(file,frequency = 10, buffer_step = 100, crossover = 1, capture = 0):
-    my_robot.capture = capture
+    my_camera.capture = capture
     yield from mv(my_robot.file, file)
     yield from mv(my_robot.frequency, frequency)
     yield from mv(my_robot.buffer_step, buffer_step)
     yield from mv(my_robot.crossover, crossover)
     yield from mv(my_robot.parse, 1)
     yield from mv(my_robot.buffer, 1)
-
-def capture(**kwargs):
-    if my_robot.capture:
-        my_camera.acquire.put(1)
 
 my_robot.checkpoint.subscribe(capture)
